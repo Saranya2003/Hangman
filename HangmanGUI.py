03948e1b55d7ff2b3965942c92be5edb3f8adaa3
Saranya2003 <62011240@KMITL.AC.TH>
2021-12-24 12:09:25 +0700

    fix vocab

--- HangmanGUI.py
+++ HangmanGUI.py
@@ -11,7 +11,7 @@ letterList = ['a','A','b','B','c','C','d','D','e','E',
             'p','P','q','Q','r','R','s','S','t','T',
             'u','U','v','V','w','W','x','X','y','Y',
             'z','Z']
-vocabList = ['Software','Security','Enginnering','Programmer','Algorithm','Data','Network','Logic']
+vocabList = ['Software','Security','Engineering','Programmer','Algorithm','Data','Network','Logic']
 
 
 #imgPaths=['1.png','2.png','3.png','4.png','5.png','6.png','7.png']
