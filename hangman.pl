ccbb90fc60c0d4a34e18410bfa20b3c0a1b4f89f
Saranya2003 <62011240@KMITL.AC.TH>
2021-12-25 12:45:08 +0700

    Update hangman.pl

--- hangman.pl
+++ hangman.pl
@@ -1,7 +1,7 @@
 % List of guessing vocab and letter
 
 vocab(['Software','Security','Engineering','Programmer','Algorithm','Data','Network','Logic']).
-letter(['a','A','b','B','c','C','d','D','e','E','f','F','g','G','h','H','i','I','j','J','k','K','l','L','m','M','n','N','o','O','p','P','q','Q','r','R','s','S','t','T','u','U','v','V','w','W','x','X','y','Y','z','Z']).
+letter(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w''x','y','z']).
 %run a game
 hangman:-
     getVocab(Ans),
